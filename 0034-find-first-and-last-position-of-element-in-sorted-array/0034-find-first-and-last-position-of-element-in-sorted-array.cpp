59fc40eac3c15b05b95ed1b5fd720ca8432d0db7
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-06-03 23:55:13 +0530

    Time: 0 ms (100%), Space: 17.5 MB (53.11%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
-    
-    //Approach-2
-    vector<int> search_stl(vector<int>& nums, int target) {
-        //first element equal to or greater than target
-        int l = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-        
-        //first element greater than target
-        int r = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
-        
-        //Why do we need this (nums[l] != target) ? 
-        //Because lower_bound returns first element which is equal or GREATER THAN target.
-        //So, in case the target is not in the list, it will return first greater element than target which will be wrong
-        if(l == nums.size() || nums[l] != target)
-            return {-1, -1};
-        
-        return {l, r-1};
+
+    int findRightMost(vector<int>& nums, int target, int n){
+        int l=0, r=n-1;
+        int right_most = -1;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(nums[mid]==target){
+                right_most=mid;
+                l=mid+1;
+            }else if(nums[mid]<target){
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return right_most;
     }
-    
+
+    int findLeftMost(vector<int>& nums, int target, int n){
+        int l = 0, r = n-1;
+        int left_most = -1;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(nums[mid]==target){
+                left_most = mid;
+                r = mid-1;
+            }else if(nums[mid]<target){
+                l = mid+1;
+            }else{
+                r = mid-1;
+            }
+        }
+        return left_most;
+    }
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        
-        return search_stl(nums, target); //Approach - 2
+        int n = nums.size();
+        int left_most = findLeftMost(nums, target, n);
+        int right_most = findRightMost(nums, target, n);
+        return {left_most, right_most};
     }
 };

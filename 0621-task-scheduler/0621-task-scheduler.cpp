3fcc85cae2adec0efc244e4eed3637f90825201c
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-06-08 12:29:24 +0530

    Time: 1919 ms (5%), Space: 459.1 MB (5%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,25 +1,59 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        priority_queue<pair<int,char>> pq;
-        unordered_map<char,int> mp;
-        for(char c : tasks) mp[c]++;
-        for(auto &pair : mp) pq.push({pair.second,pair.first});
-        int c = 0;
 
-        while(!pq.empty()){
-            int cycle = n+1;
-            vector<pair<int,char>> q;
-            while(cycle-- && !pq.empty()){
-                int f = pq.top().first;
-                int top = pq.top().second;
-                pq.pop();
-                if(f-1>0) q.push_back({f-1,top});
-                c++;
-            }
-            for(auto &pair : q) pq.push(pair);
-            if(!pq.empty()) c+= cycle+1;
+        unordered_map<char,int> freq;
+        unordered_map<char,int> freeTime;
+
+        for(char ch : tasks) {
+            freq[ch]++;
+            freeTime[ch] = 1;   // initially all tasks available
         }
-        return c;
+
+        priority_queue<pair<int,char>> pq;
+
+        for(auto &it : freq) {
+            pq.push({it.second, it.first});
+        }
+
+        int time = 1;
+
+        while(!pq.empty()) {
+
+            vector<pair<int,char>> pulled;
+            bool scheduled = false;
+
+            while(!pq.empty()) {
+
+                auto curr = pq.top();
+                pq.pop();
+
+                int cnt = curr.first;
+                char task = curr.second;
+
+                if(freeTime[task] <= time) {
+
+                    cnt--;
+
+                    if(cnt > 0)
+                        pq.push({cnt, task});
+
+                    freeTime[task] = time + n + 1;
+
+                    scheduled = true;
+                    break;
+                }
+                else {
+                    pulled.push_back(curr);
+                }
+            }
+
+            for(auto &x : pulled)
+                pq.push(x);
+
+            time++;
+        }
+
+        return time - 1;
     }
 };

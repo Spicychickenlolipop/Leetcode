afc176d928d19180baecb98f26e1514b5070e377
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-06-16 00:40:58 +0530

    Time: 55 ms (57.77%), Space: 25.6 MB (39.54%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,67 +1,45 @@
 class Solution {
 public:
+    bool valid(int i,int j,int n,int m){
+        if(i<0 || i>=n || j<0 || j>=m)
+            return false;
+        return true;
+    }
     int minimumEffortPath(vector<vector<int>>& heights) {
-
         int n = heights.size();
         int m = heights[0].size();
-
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-
-        priority_queue<
-            pair<int,pair<int,int>>,
-            vector<pair<int,pair<int,int>>>,
-            greater<pair<int,pair<int,int>>>
-        > pq;
-
-        dist[0][0] = 0;
-
-        pq.push({0,{0,0}});
-
-        int x[4] = {-1,1,0,0};
-        int y[4] = {0,0,-1,1};
-
-        while(!pq.empty()) {
-
-            auto p = pq.top();
-            pq.pop();
-
-            int effort = p.first;
-            int r = p.second.first;
-            int c = p.second.second;
-
-            if(r == n-1 && c == m-1)
-                return effort;
-
-            if(effort > dist[r][c])
-                continue;
-
-            for(int k=0;k<4;k++) {
-
-                int row = r + x[k];
-                int col = c + y[k];
-
-                if(row>=0 && row<n &&
-                   col>=0 && col<m) {
-
-                    int newEffort =
-                        max(
-                            effort,
-                            abs(heights[r][c] -
-                                heights[row][col])
-                        );
-
-                    if(newEffort < dist[row][col]) {
-
-                        dist[row][col] = newEffort;
-
-                        pq.push(
-                            {newEffort,{row,col}}
-                        );
-                    }
-                }
-            }
+        vector<vector<int>>res(n);
+        int i;
+        for(i=0;i<n;i++){
+            vector<int>t(m,INT_MAX);
+            res[i]=t;
         }
-
-        return 0;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        int x[4]={1,-1,0,0};
+        int y[4]={0,0,1,-1};
+        res[0][0]=0;
+        pq.push({0,{0,0}});
+        while(!pq.empty()){
+            pair<int,pair<int,int>>p=pq.top();
+                pq.pop();
+            int dis=p.first;
+            int row=p.second.first;
+            int col=p.second.second;
+            if(dis>res[row][col])
+                continue;
+            for(int k= 0;k<4;k++){
+                int r=row+x[k];
+                int c=col+y[k];
+                if(!valid(r,c,n,m))
+                    continue;
+                int absdiff=abs(heights[row][col]-heights[r][c]);
+                int newwt= max(absdiff,dis);
+                if(newwt<res[r][c]){
+                    res[r][c]=newwt;
+                    pq.push({newwt,{r,c}});
+                }
+            } 
+        }
+        return res[n-1][m-1];
     }
 };

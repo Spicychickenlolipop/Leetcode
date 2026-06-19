7e4933abd6b260f7482325802cc348c378e7fc4b
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-06-19 18:08:51 +0530

    Time: 0 ms (100%), Space: 9.8 MB (49.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -22,19 +22,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int>count (26,0);
+        vector<int> freq(26, 0);
         
         for(char &ch : s){
-            count[ch-'a']++;
+            freq[ch-'a']++;
         }
         for(char &ch : t){
-            count[ch-'a']--;
+            freq[ch-'a']--;
         }
 
-        bool allZeros = all_of(begin(count),end(count), [](int element){
-            return element == 0;
-        });
-        
-        return allZeros;
-    }
+        for(int x : freq) { if(x != 0) return false; } return true; }
+    
 };

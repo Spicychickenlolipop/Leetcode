75a2bfd19a8f88a7bfb2ec05d4853f0abb463f17
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-06-19 16:34:13 +0530

    Time: 0 ms (100%), Space: 9.7 MB (75.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,18 +1,40 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         int ssize = s.length();
+//         int tsize = t.length();
+//         if(ssize != tsize)
+//             return false;
+
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
+
+//         for(int i=0;i<ssize;i++){
+//             if(s[i]!=t[i])
+//                 return false;
+//         }
+//         return true;
+//     }
+// };
+
+
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int ssize = s.length();
-        int tsize = t.length();
-        if(ssize != tsize)
-            return false;
-
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-
-        for(int i=0;i<ssize;i++){
-            if(s[i]!=t[i])
-                return false;
+        vector<int>count (26,0);
+        
+        for(char &ch : s){
+            count[ch-'a']++;
         }
-        return true;
+        for(char &ch : t){
+            count[ch-'a']--;
+        }
+
+        bool allZeros = all_of(begin(count),end(count), [](int element){
+            return element == 0;
+        });
+        
+        return allZeros;
     }
 };

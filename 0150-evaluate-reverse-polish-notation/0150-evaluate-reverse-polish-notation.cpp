cc059944351bb18db97ba9d3210f00ea1c2e3ab6
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-06-21 21:37:07 +0530

    Time: 1 ms (49.29%), Space: 17.1 MB (40.64%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,50 +1,14 @@
-class Solution {
-public:
-    int evalRPN(vector<string>& tokens) {
-        stack<int>st;
-
-        unordered_map<string, function<int (int,int)>>mp={
-            {"+", [](int a, int b){return a+b;} },
-            {"-", [](int a, int b){return a-b;} },
-            {"*", [](int a, int b){return (long)a*(long)b;} },
-            {"/", [](int a, int b){return a/b;} },
-        };
-        for(string &token : tokens){
-            if(token == "+" || token == "-" || token =="*" || token == "/"){
-                int b = st.top();
-                st.pop();
-
-                int a = st.top();
-                st.pop();
-
-                int result = mp[token](a,b);
-
-                st.push(result);
-
-            }else{
-                st.push(stoi(token));
-            }
-        }
-        return st.top();
-    }
-};
-
-
-// class Solution {
+// class Solution {//using lamda    //stack is better approach use that in interview
 // public:
-//     int Operate(int a,int b,string token){
-//         if(token == "+")
-//             return a+b;
-//         if(token == "-")
-//             return a-b;
-//         if(token == "*")
-//             return (long)a*(long)b;
-//         if(token == "/")
-//             return a/b;
-//         return -1;
-//     }
 //     int evalRPN(vector<string>& tokens) {
 //         stack<int>st;
+
+//         unordered_map<string, function<int (int,int)>>mp={
+//             {"+", [](int a, int b){return a+b;} },
+//             {"-", [](int a, int b){return a-b;} },
+//             {"*", [](int a, int b){return (long)a*(long)b;} },
+//             {"/", [](int a, int b){return a/b;} },
+//         };
 //         for(string &token : tokens){
 //             if(token == "+" || token == "-" || token =="*" || token == "/"){
 //                 int b = st.top();
@@ -53,7 +17,7 @@ public:
 //                 int a = st.top();
 //                 st.pop();
 
-//                 int result = Operate(a,b,token);
+//                 int result = mp[token](a,b);
 
 //                 st.push(result);
 
@@ -64,3 +28,39 @@ public:
 //         return st.top();
 //     }
 // };
+
+
+class Solution {
+public:
+    int Operate(int a,int b,string token){
+        if(token == "+")
+            return a+b;
+        if(token == "-")
+            return a-b;
+        if(token == "*")
+            return (long)a*(long)b;
+        if(token == "/")
+            return a/b;
+        return -1;
+    }
+    int evalRPN(vector<string>& tokens) {
+        stack<int>st;
+        for(string &token : tokens){
+            if(token == "+" || token == "-" || token =="*" || token == "/"){
+                int b = st.top();
+                st.pop();
+
+                int a = st.top();
+                st.pop();
+
+                int result = Operate(a,b,token);
+
+                st.push(result);
+
+            }else{
+                st.push(stoi(token));
+            }
+        }
+        return st.top();
+    }
+};

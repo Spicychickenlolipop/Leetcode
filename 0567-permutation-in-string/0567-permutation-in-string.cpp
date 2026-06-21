e070dce1c4a1d5eff67678387c307cc1c19b4cae
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-06-21 15:45:11 +0530

    Time: 0 ms (100%), Space: 10.1 MB (46.2%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -32,7 +32,32 @@
 
 
 
-class Solution {//approach2
+// class Solution {//approach2 //TC : O((m-n)*nlogn) sc: O(n)
+// public:
+//     bool checkInclusion(string s1, string s2) {
+//         int n = s1.length();
+//         int m = s2.length();
+
+//         if(n>m){
+//             return false;
+//         }
+//         sort(begin(s1),end(s1));
+
+//         for(int i=0;i<=m-n;i++){
+//             string substring = s2.substr(i,n);
+
+//             sort(begin(substring),end(substring));
+//             if(s1 == substring){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
+
+class Solution {//sliding window //Tc : O(m+n)  sc=0(n)
 public:
     bool checkInclusion(string s1, string s2) {
         int n = s1.length();
@@ -41,25 +66,26 @@ public:
         if(n>m){
             return false;
         }
-        sort(begin(s1),end(s1));
 
-        for(int i=0;i<=m-n;i++){
-            string substring = s2.substr(i,n);
+        vector<int>s1_freq(26,0);
+        vector<int>s2_freq(26,0);
 
-            sort(begin(substring),end(substring));
-            if(s1 == substring){
+        for(char &ch : s1){
+            s1_freq[ch-'a']++;
+        }
+        int i= 0;
+        int j=0;
+        while(j<m){
+            s2_freq[s2[j]-'a']++;
+            if(j-i+1>n){
+                s2_freq[s2[i]-'a']--;
+                i++;
+            }
+            if(s1_freq == s2_freq){
                 return true;
             }
+            j++;
         }
         return false;
     }
 };
-
-
-
-// class Solution {
-// public:
-//     bool checkInclusion(string s1, string s2) {
-        
-//     }
-// };

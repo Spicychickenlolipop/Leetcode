1e194d509c3648cccf0045ef5f7b29ae4358d0a3
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-06-21 20:09:21 +0530

    Time: 48 ms (34.94%), Space: 151.4 MB (11.12%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,31 +1,31 @@
-class MinStack {
-public:
-    stack<pair<int, int>> s;
-    MinStack() {
+// class MinStack { //tc: O(1) sc: O(2*n) //good solution
+// public:
+//     stack<pair<int, int>> s;
+//     MinStack() {
         
-    }
+//     }
     
-    void push(int value) {
-        if(s.empty()){
-            s.push({value,value});
-        }else{
-            int minVal = min(value, s.top().second);
-            s.push({value,minVal});
-        }
-    }
+//     void push(int value) {
+//         if(s.empty()){
+//             s.push({value,value});
+//         }else{
+//             int minVal = min(value, s.top().second);
+//             s.push({value,minVal});
+//         }
+//     }
     
-    void pop() {
-        s.pop();
-    }
+//     void pop() {
+//         s.pop();
+//     }
     
-    int top() {
-        return s.top().first;
-    }
+//     int top() {
+//         return s.top().first;
+//     }
     
-    int getMin() {
-        return s.top().second;
-    }
-};
+//     int getMin() {
+//         return s.top().second;
+//     }
+// };
 
 /**
  * Your MinStack object will be instantiated and called as such:
@@ -35,3 +35,47 @@ public:
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
+
+
+
+
+ class MinStack { //tc: O(1) sc: O(2*n)
+public:
+    stack<long long> s;
+    long long int minVal;
+    MinStack() {
+        
+    }
+    
+    void push(int value) {
+        if(s.empty()){
+            s.push(value);
+            minVal = value;
+        }else{
+            if(value<minVal){
+                s.push((long long) 2*value-minVal);
+                minVal = value;
+            }else{
+                s.push(value);
+            }
+        }
+    }
+    
+    void pop() {
+        if(s.top()<minVal){
+            minVal = 2*minVal - s.top();
+        }
+        s.pop();
+    }
+    
+    int top() {
+        if(s.top()<minVal){
+            return minVal;
+        }
+        return s.top();
+    }
+    
+    int getMin() {
+        return minVal;
+    }
+};

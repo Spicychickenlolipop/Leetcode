84c9857d226ac4ed7b98456d58eb51baf82d15ce
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-06-21 21:36:14 +0530

    Time: 0 ms (100%), Space: 17.2 MB (14.51%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int>st;
+
+        unordered_map<string, function<int (int,int)>>mp={
+            {"+", [](int a, int b){return a+b;} },
+            {"-", [](int a, int b){return a-b;} },
+            {"*", [](int a, int b){return (long)a*(long)b;} },
+            {"/", [](int a, int b){return a/b;} },
+        };
+        for(string &token : tokens){
+            if(token == "+" || token == "-" || token =="*" || token == "/"){
+                int b = st.top();
+                st.pop();
+
+                int a = st.top();
+                st.pop();
+
+                int result = mp[token](a,b);
+
+                st.push(result);
+
+            }else{
+                st.push(stoi(token));
+            }
+        }
+        return st.top();
+    }
+};
+
+
+// class Solution {
+// public:
+//     int Operate(int a,int b,string token){
+//         if(token == "+")
+//             return a+b;
+//         if(token == "-")
+//             return a-b;
+//         if(token == "*")
+//             return (long)a*(long)b;
+//         if(token == "/")
+//             return a/b;
+//         return -1;
+//     }
+//     int evalRPN(vector<string>& tokens) {
+//         stack<int>st;
+//         for(string &token : tokens){
+//             if(token == "+" || token == "-" || token =="*" || token == "/"){
+//                 int b = st.top();
+//                 st.pop();
+
+//                 int a = st.top();
+//                 st.pop();
+
+//                 int result = Operate(a,b,token);
+
+//                 st.push(result);
+
+//             }else{
+//                 st.push(stoi(token));
+//             }
+//         }
+//         return st.top();
+//     }
+// };

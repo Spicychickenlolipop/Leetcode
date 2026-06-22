93c2a19dc7d5534236c53ec3ff1176f51eb577f4
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-06-22 21:40:55 +0530

    Time: 0 ms (100%), Space: 19.5 MB (27.19%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -8,23 +8,61 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//approach1
+
+// class Solution {//recursive solution
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         if(list1==NULL){
+//             return list2;
+//         }
+//         if(list2==NULL){
+//             return list1;
+//         }
+//         ListNode* result;
+//         if(list1->val<list2->val){
+//             result =list1;
+//             result->next = mergeTwoLists(list1->next,list2);
+//         }else{
+//             result = list2;
+//             result->next = mergeTwoLists(list1,list2->next);
+//         }
+//         return result;
+//     }
+// };
+
+
+
+
 class Solution {//recursive solution
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL){
-            return list2;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* curr = dummy;
+
+        while(list1 != nullptr && list2 != nullptr) {
+
+            if(list1->val <= list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+
+            curr = curr->next;
         }
-        if(list2==NULL){
-            return list1;
+
+        if(list1 != nullptr) {
+            curr->next = list1;
         }
-        ListNode* result;
-        if(list1->val<list2->val){
-            result =list1;
-            result->next = mergeTwoLists(list1->next,list2);
-        }else{
-            result = list2;
-            result->next = mergeTwoLists(list1,list2->next);
+
+        if(list2 != nullptr) {
+            curr->next = list2;
         }
-        return result;
+
+        return dummy->next;
     }
 };

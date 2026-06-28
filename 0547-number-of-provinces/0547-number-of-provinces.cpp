593e121a432396e2f640166b189f22ad717a8b68
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-06-28 20:57:29 +0530

    Time: 0 ms (100%), Space: 19.3 MB (92.91%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,43 +1,65 @@
+// //Approach-1 : [ Using DFS - Creating our own Map as graph ]
+// class Solution {
+// public:
+//     void dfs(unordered_map<int,vector<int>> &adj, int u, vector<bool>& visited){
+//         visited[u]=true;
+
+//         //visit neighbours
+//         for(int &v : adj[u]){
+//             if(!visited[v]){
+//                 dfs(adj,v,visited);
+//             }
+//         }
+//     }
+
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int n = isConnected.size();
+//         unordered_map<int, vector<int>>adj;
+//         for(int i=0; i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 if(isConnected[i][j]==1){
+//                     adj[i].push_back(j);
+//                     adj[j].push_back(i);
+//                 }
+//             }
+//         }
+//         vector<bool>visited(n,false);
+//         int count=0;
+//         for(int i=0;i<n;i++){
+//             if(!visited[i]){
+//                 count++;
+//                 dfs(adj,i,visited);
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+//Approach-2 : [ Using DFS - Using given input graph]
 class Solution {
 public:
-    void dfs(unordered_map<int,vector<int>> &adj, int u, vector<bool>& visited){
+    int n;
+    void dfs(vector<vector<int>> &adj, int u, vector<bool>& visited){
         visited[u]=true;
 
-        //visit neighbours
-        for(int &v : adj[u]){
-            if(!visited[v]){
+        //visited neighbours
+        for(int v=0;v<n;v++){
+            if(adj[u][v]==1 && !visited[v]){
                 dfs(adj,v,visited);
             }
         }
     }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        unordered_map<int, vector<int>>adj;
-        for(int i=0; i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
-        }
-        vector<bool>visited(n,false);
-        int count=0;
+        n = isConnected.size();
+        vector<bool> visited(n,false);
+        int count =0;
         for(int i=0;i<n;i++){
             if(!visited[i]){
                 count++;
-                dfs(adj,i,visited);
+                dfs(isConnected,i, visited);
             }
         }
         return count;
     }
 };
-
-
-// class Solution {
-// public:
-//     int findCircleNum(vector<vector<int>>& isConnected) {
-        
-//     }
-// };

922319ecf5e90ae0ed87599bbeb51cfc12c29007
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-06-28 21:31:30 +0530

    Time: 1 ms (47%), Space: 22 MB (5.3%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -70,9 +70,58 @@
 
 
 ///////////////////////////////BFS////////////////////////
+
+//Approach-3 : [ Using BFS : Creating our own Map as graph ]
+
+// class Solution{
+// public:
+//     void bfs(unordered_map<int,vector<int>>&adj,int u, vector<bool>& visited){
+//         queue<int>que;
+
+//         que.push(u);
+//         visited[u]=true;
+//         while(!que.empty()){
+//             int curr= que.front();
+//             que.pop();
+
+//             for(int &v : adj[curr]){
+//                 if(!visited[v])
+//                     bfs(adj,v,visited);
+//             }
+//         }
+//     }
+//     int findCircleNum(vector<vector<int>>& isConnected){
+//         int n = isConnected.size();
+
+//         unordered_map<int, vector<int>>adj;
+//         //make graph
+//         for(int i=0;i<n;i++){
+//             for(int j = 0; j<n;j++){
+//                 if(isConnected[i][j]==1){
+//                     adj[i].push_back(j);
+//                     adj[j].push_back(i);
+//                 }
+//             }
+//         }
+//         vector<bool> visited(n,false);
+//         int count=0;
+
+//         for(int i=0;i<n;i++){
+//             if(!visited[i]){
+//                 bfs(adj,i,visited);
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+//Approach-4 : [ Using BFS - Using given input graph]
+
 class Solution{
 public:
-    void bfs(unordered_map<int,vector<int>>&adj,int u, vector<bool>& visited){
+    int n;
+    void bfs(vector<vector<int>>& isConnected,int u, vector<bool>& visited){
         queue<int>que;
 
         que.push(u);
@@ -80,38 +129,25 @@ public:
         while(!que.empty()){
             int curr= que.front();
             que.pop();
-
-            for(int &v : adj[curr]){
-                if(!visited[v])
-                    bfs(adj,v,visited);
+            //neighbours
+            for(int v = 0; v<n;v++){
+                if(!visited[v] && isConnected[u][v]==1)
+                    bfs(isConnected,v,visited);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected){
-        int n = isConnected.size();
+         n = isConnected.size();
 
-        unordered_map<int, vector<int>>adj;
-        //make graph
-        for(int i=0;i<n;i++){
-            for(int j = 0; j<n;j++){
-                if(isConnected[i][j]==1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
-        }
         vector<bool> visited(n,false);
         int count=0;
 
         for(int i=0;i<n;i++){
             if(!visited[i]){
-                bfs(adj,i,visited);
+                bfs(isConnected,i,visited);
                 count++;
             }
         }
         return count;
     }
 };
-
-
-

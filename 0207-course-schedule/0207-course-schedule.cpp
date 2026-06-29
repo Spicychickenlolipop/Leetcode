9fbbade77a8728a385d9c376cf28bd03ad40e257
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-06-29 21:26:29 +0530

    Time: 7 ms (45.35%), Space: 20.1 MB (32.68%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,46 +1,99 @@
-//Approach-1 (Using BFS Cycle Check - Kahn's Algorithm (Topological Sort)
+// //Approach-1 (Using BFS Cycle Check - Kahn's Algorithm (Topological Sort)
+// class Solution {
+// public:
+
+//     bool topologicalSortCheck(unordered_map<int, vector<int>>&adj,int n,vector<int>&indegree){
+//         queue<int>que;
+//         int count = 0;
+//         for(int i=0;i<n;i++){
+//             if(indegree[i]==0){
+//                 count++;
+//                 que.push(i);
+//             }
+//         }
+//         while(!que.empty()){
+//             int u = que.front();
+//             que.pop();
+//             for(int &v : adj[u]){
+//                 indegree[v]--;
+//                 if(indegree[v]==0){
+//                     count++;
+//                     que.push(v);
+//                 }
+//             }
+//         }
+//         if(count==n)//I was able to visite all nodes(course)
+//             return true;//i.e. I was able to finish all courses
+//         return false;//means there was a cycle and I couldn't complete all courses
+//     }
+
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//         unordered_map<int, vector<int>> adj;
+//         vector<int> indegree(numCourses,0);//kahn's algo
+
+//         for(auto &vec : prerequisites){
+//             int a = vec[0];
+//             int b = vec[1];
+
+//             //b-->a
+//             adj[b].push_back(a);
+//             //arrow ja raha hai 'a' me
+//             indegree[a]++;
+//         }
+//         //if cycle is present, not possible
+//         return topologicalSortCheck(adj, numCourses,indegree);
+//     }
+// };
+
+
+
+
+//Approach-2 (Using DFS Cycle Check)
+
 class Solution {
 public:
 
-    bool topologicalSortCheck(unordered_map<int, vector<int>>&adj,int n,vector<int>&indegree){
-        queue<int>que;
-        int count = 0;
-        for(int i=0;i<n;i++){
-            if(indegree[i]==0){
-                count++;
-                que.push(i);
-            }
+    bool isCycleDFS(unordered_map<int, vector<int>> &adj, int u, vector<bool>&visited, vector<bool>& inRecursion){
+        visited[u]=true;
+        inRecursion[u]=true;
+
+        for(int &v : adj[u]){
+            //if not visited, then hit DFS on v
+            if(!visited[v]&& isCycleDFS(adj, v, visited, inRecursion))
+                return true;//cycle to hai
+            else if(inRecursion[v]==true)
+                return true;
         }
-        while(!que.empty()){
-            int u = que.front();
-            que.pop();
-            for(int &v : adj[u]){
-                indegree[v]--;
-                if(indegree[v]==0){
-                    count++;
-                    que.push(v);
-                }
-            }
-        }
-        if(count==n)
-            return true;
-        return false;
+        inRecursion[u]=false;
+        return false;//no cycle
     }
 
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, vector<int>> adj;
-        vector<int> indegree(numCourses,0);//kahn's algo
+        unordered_map<int, vector<int>>adj;
+
+        vector<bool>visited(numCourses,false);
+        vector<bool>inRecursion(numCourses,false);
 
         for(auto &vec : prerequisites){
             int a = vec[0];
             int b = vec[1];
 
-            //b-->a
+            //b--->a
             adj[b].push_back(a);
-            //arrow ja raha hai 'a' me
-            indegree[a]++;
         }
-        //if cycle is present, not possible
-        return topologicalSortCheck(adj, numCourses,indegree);
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]&& isCycleDFS(adj, i, visited, inRecursion))
+                return false;//course complete nahi kar sakte
+        }
+        return true;//complete kar sakte hain sare courses
     }
 };
+
+
+
+
+
+
+
+
+

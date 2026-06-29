a6e929b917f4695e47db5e6672bcc18f533854fa
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-06-29 10:32:19 +0530

    Time: 0 ms (100%), Space: 11.8 MB (33.74%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,9 +1,43 @@
+// Approach 1: Using string::find() (Simplest)
+// Time Complexity: O(n × m × k)
+// Space Complexity: O(1)
+// class Solution {
+// public:
+//     int numOfStrings(vector<string>& patterns, string word) {
+//         int count = 0;
+//         for(int i=0;i<patterns.size();i++){
+//             if(word.find(patterns[i])!=-1)
+//                 count++;
+//         }
+//         return count;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
     int numOfStrings(vector<string>& patterns, string word) {
         int count = 0;
-        for(int i=0;i<patterns.size();i++){
-            if(word.find(patterns[i])!=-1)
+        for(string pattern : patterns){
+            int m = pattern.size();
+            if(m > word.size())
+            continue;
+            bool found = false;
+
+            for(int i = 0; i<=word.size()-m;i++){
+                int j = 0;
+                while(j < m && word[i + j] == pattern[j]) {
+                    j++;
+                }
+                if(j==m){
+                    found = true;
+                    break;
+                }
+            }
+            if(found)
                 count++;
         }
         return count;

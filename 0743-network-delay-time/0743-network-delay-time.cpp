9925d0ffb84af572f13a7887afb26e846dc0213b
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-07-03 16:04:11 +0530

    Time: 92 ms (31.21%), Space: 44.3 MB (68.3%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,40 +1,112 @@
+// class Solution {
+// public:
+//     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+//         vector<vector<pair<int,int>>>a(n+1);
+//         for(int i=0;i<times.size();i++){
+//             int s = times[i][0];
+//             int d = times[i][1];
+//             int w = times[i][2];
+//             a[s].push_back({d,w});
+//         } 
+//         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+
+//         vector<int>dist(n+1,INT_MAX);
+//         dist[k]=0;
+//         pq.push({0,k});
+//         while(!pq.empty()){
+//             pair<int,int>p=pq.top();
+//             pq.pop();
+//             int d = p.first;
+//             int node = p.second;
+//             if(d>dist[node])
+//                 continue;
+//             for(int j=0;j<a[node].size();j++){
+//                 int neigh=a[node][j].first;
+//                 int wt = a[node][j].second;
+//                 if(d+wt<dist[neigh]){
+//                     dist[neigh]=d+wt;
+//                     pq.push({d+wt,neigh});
+//                 }
+//             }
+//         }
+//         int ans =0;
+//         for(int i = 1; i<=n;i++){
+//             if(dist[i]==INT_MAX)
+//                 return -1;
+//             ans=max(ans,dist[i]);
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
+
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>>a(n+1);
-        for(int i=0;i<times.size();i++){
-            int s = times[i][0];
-            int d = times[i][1];
-            int w = times[i][2];
-            a[s].push_back({d,w});
-        } 
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        unordered_map<int, vector<pair<int,int>>>adj;
+        for(auto &vec:times){
+            int u = vec[0];
+            int v = vec[1];
+            int w = vec[2];
 
-        vector<int>dist(n+1,INT_MAX);
-        dist[k]=0;
+            adj[u].push_back({v,w});
+        }
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+        
+        vector<int> result(n+1,INT_MAX);
+
+        result[k]=0;
         pq.push({0,k});
+
         while(!pq.empty()){
-            pair<int,int>p=pq.top();
+            int d = pq.top().first;
+            int node = pq.top().second;
             pq.pop();
-            int d = p.first;
-            int node = p.second;
-            if(d>dist[node])
-                continue;
-            for(int j=0;j<a[node].size();j++){
-                int neigh=a[node][j].first;
-                int wt = a[node][j].second;
-                if(d+wt<dist[neigh]){
-                    dist[neigh]=d+wt;
-                    pq.push({d+wt,neigh});
+
+            for(auto &vec : adj[node]){
+                int adjNode = vec.first;
+                int dist = vec.second;
+
+                if(d+dist <result[adjNode]){
+                    result[adjNode]=d+dist;
+                    pq.push({d+dist, adjNode});
                 }
             }
+        }        
+        int ans = INT_MIN;
+        for(int i=1;i<=n;i++){
+            ans= max(ans, result[i]);
         }
-        int ans =0;
-        for(int i = 1; i<=n;i++){
-            if(dist[i]==INT_MAX)
-                return -1;
-            ans=max(ans,dist[i]);
-        }
-        return ans;
+        return ans == INT_MAX ? -1 : ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

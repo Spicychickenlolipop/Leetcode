5017ae66a79486002a7a4051567d41f8c8791704
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-07-07 21:25:03 +0530

    Time: 0 ms (100%), Space: 9 MB (51.79%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,53 +1,53 @@
-//Approach-1
-//T.C : O(d), d = number of digits in n
-//S.C : O(d), d = number of digits in n
-class Solution {
-public:
-    typedef long long ll;
-
-    ll sumAndMultiply(int n) {
-        ll x   = 0;
-        ll sum = 0;
-
-        for(char &ch : to_string(n)) {
-            if(ch != '0') {
-                sum += ch - '0';
-                x = x*10 + (ch - '0');
-            }
-        }
-
-        return x * sum;
-
-    }
-};
-
-
-// //Approach-2
+// //Approach-1
 // //T.C : O(d), d = number of digits in n
-// //S.C : O(1)
+// //S.C : O(d), d = number of digits in n
 // class Solution {
 // public:
 //     typedef long long ll;
 
 //     ll sumAndMultiply(int n) {
-//         ll x = 0;
+//         ll x   = 0;
 //         ll sum = 0;
-//         ll pow10 = 1;
 
-//         while (n > 0) {
-//             int digit = n % 10;
-
-//             sum += digit;
-
-//             if (digit > 0) {
-//                 x += digit * pow10;
-//                 pow10 *= 10;
+//         for(char &ch : to_string(n)) {
+//             if(ch != '0') {
+//                 sum += ch - '0';
+//                 x = x*10 + (ch - '0');
 //             }
-
-//             n /= 10;
 //         }
 
 //         return x * sum;
 
 //     }
 // };
+
+
+//Approach-2
+//T.C : O(d), d = number of digits in n
+//S.C : O(1)
+class Solution {
+public:
+    typedef long long ll;
+
+    ll sumAndMultiply(int n) {
+        ll x = 0;
+        ll sum = 0;
+        ll pow10 = 1;
+
+        while (n > 0) {
+            int digit = n % 10;
+
+            sum += digit;
+
+            if (digit > 0) {
+                x += digit * pow10;
+                pow10 *= 10;
+            }
+
+            n /= 10;
+        }
+
+        return x * sum;
+
+    }
+};

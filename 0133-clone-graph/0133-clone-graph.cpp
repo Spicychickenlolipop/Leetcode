3b7eeb27a13ee991e92cc49e62e7dd18c9a4fdd7
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-07-24 20:10:26 +0530

    Time: 3 ms (67.79%), Space: 12.2 MB (50.17%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,56 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*, Node*>mp;
+
+    void DFS(Node* node, Node* clone_node){
+        for(Node* n : node->neighbors){
+            if(mp.find(n)==mp.end()){
+                Node* clone = new Node(n->val);
+                mp[n] = clone;
+                clone_node->neighbors.push_back(clone);
+
+                DFS(n, clone);
+            }else{
+                clone_node->neighbors.push_back(mp[n]);
+            }
+        }
+    }
+
+    Node* cloneGraph(Node* node) {
+        if(node==NULL)
+            return NULL;
+
+        mp.clear();
+        //clone the given node
+        Node* clone_node = new Node(node->val);
+        //Now, clone its neighbors and recursively their neighbors
+        //But if a node reappears, then we need to access that cloned node . So, store them in a map<Node*, Node*>
+
+        mp[node] = clone_node;
+
+        DFS(node, clone_node);
+
+        return clone_node;
+    }
+};

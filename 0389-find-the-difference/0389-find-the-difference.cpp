7d349b0e2c0a3a4f382349287096b652a19e876c
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-07-27 09:43:34 +0530

    Time: 0 ms (100%), Space: 9.2 MB (78.89%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -17,18 +17,42 @@
 // };
 
 
+// class Solution {
+// public:
+//     char findTheDifference(string s, string t) {
+//        int sum_s = 0;
+//        for(char &ch : s){
+//         sum_s += ch;
+//        }
+//        int sum_t = 0;
+//        for(char &ch : t){
+//         sum_t += ch;
+//        }
+
+//        return (char)(sum_t-sum_s);
+//     }
+// };
+
+
+
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-       int sum_s = 0;
-       for(char &ch : s){
-        sum_s += ch;
-       }
-       int sum_t = 0;
-       for(char &ch : t){
-        sum_t += ch;
-       }
+        int XOR = 0;
 
-       return (char)(sum_t-sum_s);
+        for(char &ch : s){
+            XOR ^= ch;
+        }
+        for(char &ch : t){
+            XOR ^= ch;
+        }
+
+        return (char)XOR;
     }
 };
+
+
+
+
+
+

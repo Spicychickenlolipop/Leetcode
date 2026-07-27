9bc362fca8079ec0202784d3fd200bfd41001c47
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-07-27 09:41:15 +0530

    Time: 0 ms (100%), Space: 9.1 MB (78.89%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,17 +1,34 @@
+// class Solution {
+// public:
+//     char findTheDifference(string s, string t) {
+//         unordered_map<char, int>mp;
+//         for(char &ch : s){
+//             mp[ch]++;
+//         }
+//         for(char &ch : t){
+//             mp[ch]--;
+
+//             if(mp[ch]<0){
+//                 return ch;
+//             }
+//         }
+//         return 'x';
+//     }
+// };
+
+
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char, int>mp;
-        for(char &ch : s){
-            mp[ch]++;
-        }
-        for(char &ch : t){
-            mp[ch]--;
+       int sum_s = 0;
+       for(char &ch : s){
+        sum_s += ch;
+       }
+       int sum_t = 0;
+       for(char &ch : t){
+        sum_t += ch;
+       }
 
-            if(mp[ch]<0){
-                return ch;
-            }
-        }
-        return 'x';
+       return (char)(sum_t-sum_s);
     }
 };

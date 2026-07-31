0b492da555643bf247c96237b0d36d605188cc1a
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-07-31 08:35:49 +0530

    Time: 0 ms (100%), Space: 8.6 MB (23.08%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,19 +1,51 @@
+// class Solution {
+// public:
+//     int minimumPushes(string word) {
+//         int result = 0;
+//         unordered_map<int, int> mp;
+
+//         int assign_key = 2;
+//         for(char &ch : word){
+//             if(assign_key > 9){
+//                 assign_key = 2;
+//             }
+
+//             mp[assign_key]++;
+//             result += mp[assign_key];
+
+//             assign_key++;
+//         }
+//         return result;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     int minimumPushes(string word) {
-        int result = 0;
-        unordered_map<int, int> mp;
+        vector<int>mp(26,0);
 
-        int assign_key = 2;
         for(char &ch : word){
-            if(assign_key > 9){
-                assign_key = 2;
-            }
+            mp[ch-'a']++;  
+        }
 
-            mp[assign_key]++;
-            result += mp[assign_key];
+        sort(begin(mp),end(mp),greater<int>());
 
-            assign_key++;
+        int result = 0;
+
+        for(int i = 0; i<26;i++){
+            int freq = mp[i];
+            int press = i/8+1;
+
+            result += press * freq;
         }
         return result;
     }

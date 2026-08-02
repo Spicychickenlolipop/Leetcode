393597d34e3c986a1f52bfc6f88f66ea09505b8e
chickenloli_pop <158480572+Spicychickenlolipop@users.noreply.github.com>
2026-08-02 09:34:51 +0530

    Time: 0 ms (100%), Space: 28.1 MB (72.17%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,17 +1,38 @@
-class Solution {
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         int count = 0;
+//         int maj = NULL;
+
+//         for(int i = 0;i<n;i++){
+//             if(count == 0){
+//                 count = 1;
+//                 maj = nums[i];
+//             }
+//             else if(nums[i]==maj){
+//                 count++;
+//             }else{
+//                 count--;
+//             }
+//         }
+//         return maj;
+//     }
+// };
+
+class Solution{
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
         int count = 0;
         int maj = NULL;
 
-        for(int i = 0;i<n;i++){
-            if(count == 0){
-                count = 1;
-                maj = nums[i];
-            }
-            else if(nums[i]==maj){
+        for(int i = 0; i<n;i++){
+            if(nums[i]==maj){
                 count++;
+            }else if(count == 0){
+                maj = nums[i];
+                count = 1;
             }else{
                 count--;
             }
